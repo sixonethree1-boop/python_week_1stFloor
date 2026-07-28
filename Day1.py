0d08525d8a335ae33651602eabc21d2acbac4933
PAO <pao@iwc.com>
2026-07-28 14:24:48 +0800

    Initial upload

---
+++ Day1.py
@@ -0,0 +1,55 @@
+#Variable and Data Types
+
+# nickname = "pao" #string
+# print(nickname)
+
+# age = 29   #integer
+# print(age)
+
+# temperature = 32.1 #float (decimal)
+# print(temperature)
+
+# is_marine = True  #boolean
+# print(is_marine)
+
+# #How to check data type
+# print(type("pao"))
+# print(type(29))
+# print(type(32.1))
+# print(type(True))
+
+# #How to manipulate strings
+# msg = "Information Warfare Center"
+# print(msg.upper())
+# print(msg.lower())
+# print(msg.count("a"))
+# print(msg.split())
+
+# #Combination of data types and input from user
+name = input("What is your name? ")
+print("Hello " + name + "!")
+
+age = input("How old are you? ")
+print("You are " + age + " years old.")
+age_int = int(age)
+next_year = age_int + 1
+print("Next year, you'll be " + str(next_year) + " years old.")
+
+# create input based query that asks the user for their name, 
+# python greets the user "hello.. input"
+# python asks the user for his/her year of birth, 
+# converts the year of birth to integer
+# calculate the user's year of mandatory retirement
+# gives back the result in string "You will retire by....."
+
+# UNDERSTAND THIS CODE
+char = input("Enter a character: ")
+if char.isupper():
+    print("Uppercase")
+elif char.islower():
+    print("Lowercase")
+else:
+    print("Not a letter")
+    
+#advance reading on loops  and conditional statemet
+#clone the github repo to be sent to the signal group
---
+++ ZenOfPython.py
@@ -0,0 +1,94 @@
+# DAY 1- ZEN OF PYTHON
+    # Beautiful is better than ugly.
+    
+            # X=10
+            # Y=20
+            # print(X+Y)
+
+            # x = 10
+            # y = 20
+            # print(x+y)
+            
+    #Explicit is better than implicit.
+
+            # a = "Pvt Austria"
+            # print(a)
+
+            # name = "Pvt Austria"
+            # print(name)
+    
+    # Simple is better than complex.
+
+            # result = 5+5
+            # print(result)
+            
+    #Complex is better than complicated.
+            # if  (10 > 5) == True:
+            #     print("Correct")
+
+            # if 10 > 5:
+            #     print("Correct")
+            
+    # Flat is better than nested.
+            # age = 20
+            # has_id = True
+
+            # if age >= 18:
+            #     if has_id:
+            #         print("Allowed")
+
+            # if age >= 18 and has_id:
+            #     print("Allowed")
+            
+    # Sparse is better than dense.
+    # Readability counts.
+
+            # name = "Panganiban"
+            # rank = "Cpl"
+            # unit = "2MBde"
+            # print(rank, name, unit)
+            
+    # Special cases aren't special enough to break the rules.
+
+        # training = "Python"
+        # duration = "1 Month"
+        # school = "IWU"
+        # nr_of_students = 20
+
+        # print(training, duration, school, nr_of_students)
+        
+    # Although practicality beats purity.
+    # Errors should never pass silently.
+    # Unless explicitly silenced.
+        # Score = 100
+        # try:
+        #     print(Score)
+        # except:
+        #     print("Score not found")
+        
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+
+        # name = input("Enter rank then family name :")
+        # print("Hello " + name + " !")
+
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+
+# Namespaces are one honking great idea -- let's do more of those!
+    # def battalion():
+    #     s1 = "CPT JUAN"
+    #     print(s1)
+
+    # def brigade():
+    #     s1 = "CPT SANTOS"
+    #     print(s1)
+        
+    # battalion()
+    # brigade()
+
+    #global namespace
+    #local namespace
